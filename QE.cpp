dc841af2f8c9f056e4ba58b2cbe5c2ae146fc1f6
Kefirock <38141695+Kefirock@users.noreply.github.com>
2019-12-11 15:01:22 +0400

    Update QE.cpp

--- QE.cpp
+++ QE.cpp
@@ -6,13 +6,64 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+double calculateD (double a, double b, double c)
+{
+    return pow(b,2) - 4*a*c;
+}
+
+int calculateX(double a, double b, double c, double *x1, double *x2)
+{
+    double D = calculateD(a,b,c);
+    if (D > 0)
+    {
+        *x1 = (-b + sqrt(D)) / (2*a);
+        *x2 = (-b - sqrt(D)) / (2*a);
+
+        return 2;
+    }
+
+    if (D == 0)
+    {
+        *x1 = (-b) / (2*a);
+
+        return 1;
+    }
+
+    return 0;
+}
+
+void print(double *x1, double *x2, int roots)
+{
+
+    cout << "Our ninjas did the math." << endl;
+    cout << "" << endl;
+
+    if (roots == 0)
+    {
+        cout << "No roots. But here's a cookie for you." << endl;
+    }
+
+    if (roots == 1)
+    {
+        cout << "Single root: "<< *x1 << endl;
+    }
+
+    if (roots == 2)
+    {
+        cout << "First root: "<< *x1 << endl;
+        cout << "Second root: "<< *x2 << endl;
+    }
+
+}
+
 int main()
 {
     double a;
     double b;
     double c;
-    double x;
-    double D;
+    double x1;
+    double x2;
+    int roots;
 
     while (true)
     {
@@ -25,32 +76,8 @@ int main()
         cout << "c:";
         cin >> c;
 
-        D = pow(b,2) - 4*a*c;
-
-        cout << "" << endl;
-        cout << "Our ninjas did the math." << endl;
-        cout << "" << endl;
-
-        if (D > 0)
-            {
-                x = (-b + sqrt(D)) / (2*a);
-                cout << "First root: "<< x << endl;
-
-                x = (-b - sqrt(D)) / (2*a);
-                cout << "Second root: "<< x << endl;
-
-                continue;
-            }
-
-        else if (D == 0)
-            {
-                x = (-b) / (2*a);
-                cout << "Single root: "<< x << endl;
-                continue;
-            }
-
-        cout << "No roots. But here's a cookie for you." << endl;
-
+        roots = calculateX(a,b,c, &x1, &x2);
+        print(&x1, &x2, roots);
     }
 
     return 0;
